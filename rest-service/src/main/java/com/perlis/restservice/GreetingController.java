d93703a272850c5aa412251d1c507718439eeb15
Julian_Munz <munzjulian@gmail.com>
2021-10-18 13:24:34 +0200

    CORS

--- rest-service/src/main/java/com/perlis/restservice/GreetingController.java
+++ rest-service/src/main/java/com/perlis/restservice/GreetingController.java
@@ -11,6 +11,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,7 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/scrape")
 	public String scrape(@RequestParam(value = "url", defaultValue = "https://www.google.com/") String url) {
 		Document doc = null;
@@ -71,68 +73,46 @@ public class GreetingController {
 		// scraping failed
 		return null;
 	}
-	
-	
-	
-	
-@GetMapping(value = "/getPredictions")
-public void getPredictions() throws GeneralSecurityException, IOException{	
- HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-    Discovery discovery = new Discovery.Builder(httpTransport, jsonFactory, null).build();
 
-    RestDescription api = discovery.apis().getRest("ml", "v1").execute();
-    RestMethod method = api.getResources().get("projects").getMethods().get("predict");
+	@CrossOrigin(origins = "http://localhost:8080")
+	@GetMapping(value = "/getPredictions")
+	public void getPredictions() throws GeneralSecurityException, IOException{
+		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		Discovery discovery = new Discovery.Builder(httpTransport, jsonFactory, null).build();
 
-    JsonSchema param = new JsonSchema();
-    String projectId = "wide-river-326910";
-    // You should have already deployed a model and a version.
-    // For reference, see https://cloud.google.com/ml-engine/docs/deploying-models.
-    String modelId = "Impact";
-    String versionId = "Version2";
-    param.set(
-        "name", String.format("projects/%s/models/%s/versions/%s", projectId, modelId, versionId));
-    GenericUrl url =
-        new GenericUrl(UriTemplate.expand(api.getBaseUrl() + method.getPath(), param, true));
-    System.out.println(url);
+		RestDescription api = discovery.apis().getRest("ml", "v1").execute();
+		RestMethod method = api.getResources().get("projects").getMethods().get("predict");
 
-    String contentType = "application/json";
-    File requestBodyFile = new File("main.json");
-    HttpContent content = new FileContent(contentType, requestBodyFile);
-	System.out.println(requestBodyFile);
-    System.out.println(content.getLength());
-	System.out.println(content);
-	
-    List<String> scopes = new ArrayList<>();
-    scopes.add("https://www.googleapis.com/auth/cloud-platform");
+		JsonSchema param = new JsonSchema();
+		String projectId = "wide-river-326910";
+		// You should have already deployed a model and a version.
+		// For reference, see https://cloud.google.com/ml-engine/docs/deploying-models.
+		String modelId = "Impact";
+		String versionId = "Version2";
+		param.set(
+				"name", String.format("projects/%s/models/%s/versions/%s", projectId, modelId, versionId));
+		GenericUrl url =
+				new GenericUrl(UriTemplate.expand(api.getBaseUrl() + method.getPath(), param, true));
+		System.out.println(url);
 
-    GoogleCredentials credential = GoogleCredentials.getApplicationDefault().createScoped(scopes);
-    HttpRequestFactory requestFactory =
-        httpTransport.createRequestFactory(new HttpCredentialsAdapter(credential));
-    HttpRequest request = requestFactory.buildRequest(method.getHttpMethod(), url, content);
+		String contentType = "application/json";
+		File requestBodyFile = new File("main.json");
+		HttpContent content = new FileContent(contentType, requestBodyFile);
+		System.out.println(requestBodyFile);
+		System.out.println(content.getLength());
+		System.out.println(content);
 
-    String response = request.execute().parseAsString();
-    System.out.println(response);	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		List<String> scopes = new ArrayList<>();
+		scopes.add("https://www.googleapis.com/auth/cloud-platform");
+
+		GoogleCredentials credential = GoogleCredentials.getApplicationDefault().createScoped(scopes);
+		HttpRequestFactory requestFactory =
+				httpTransport.createRequestFactory(new HttpCredentialsAdapter(credential));
+		HttpRequest request = requestFactory.buildRequest(method.getHttpMethod(), url, content);
+
+		String response = request.execute().parseAsString();
+		System.out.println(response);
+
+	}
 }
--- rest-service/src/main/java/com/perlis/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/perlis/restservice/RestServiceApplication.java
@@ -2,6 +2,9 @@ package com.perlis.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestServiceApplication {
@@ -10,4 +13,14 @@ public class RestServiceApplication {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/scrape").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/getPredictions").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
