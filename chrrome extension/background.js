17cd99aa3f8a53112d28d4a2564011843030d5c3
Julian_Munz <munzjulian@gmail.com>
2021-10-20 17:45:20 +0200

    backround

--- chrrome extension/background.js
+++ chrrome extension/background.js
@@ -2,7 +2,20 @@
 chrome.runtime.onMessage.addListener(function(message, sender) {
     if (!message.run) return;
 
-    fetch('http://localhost:8080').then(r => r.text()).then(result => {
-    // Result now contains the response text, do what you want...
-    })
+    const url = message.url;
+    console.log('Scraping url: ' + url);
+    const apiCall = 'http://localhost:8080/api/scrape?url=' + url;
+    console.log('API call made: ' + apiCall);
+    fetch(apiCall).then( function(res) {
+      //wait for response
+      if (res.status !== 200) {
+        console.log({name:'Error', num_par: '0'});
+        return;
+      }
+      res.json().then(function(data) {
+        console.log(data);
+        //data is the list of jsons
+      });
+    });
+    return true;
 });
--- chrrome extension/popup.js
+++ chrrome extension/popup.js
@@ -3,20 +3,18 @@ let colorDivs = document.getElementById("colorDivs");
 
 colorDivs.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: showColoring,
     });
   });
-  
+
   // The body of this function will be executed as a content script inside the
   // current page
   function showColoring() {
     var text = "true";
-    chrome.runtime.sendMessage({run: true, data:{
-      text
-    }});
+    chrome.runtime.sendMessage({run: true, data:{text}, url: window.location.href});
     document.getElementById("content").style.backgroundColor = "blue";
     document.getElementById("mw-head-base").style.backgroundColor = "yellow";
   }
