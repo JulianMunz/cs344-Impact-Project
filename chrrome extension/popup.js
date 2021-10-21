4ac14950b07abf75d9ff0b2ec88ead60e6506015
Thaddeus Biernacki <69869699+Thaddeus-Biernacki@users.noreply.github.com>
2021-10-21 09:27:00 +0200

    Update popup.js

--- chrrome extension/popup.js
+++ chrrome extension/popup.js
@@ -14,7 +14,18 @@ colorDivs.addEventListener("click", async () => {
   // current page
   function showColoring() {
     var text = "true";
-    chrome.runtime.sendMessage({run: true, data:{text}, url: window.location.href});
-    document.getElementById("content").style.backgroundColor = "blue";
-    document.getElementById("mw-head-base").style.backgroundColor = "yellow";
+    chrome.runtime.sendMessage({run: true, data:{text}, url: window.location.href}, function(response) {
+      console.log(response);
+      var all = document.querySelectorAll("*");
+      for (var i = 0, max=all.length; i < max; i++) {
+        if (response[i] == "none") {
+
+        } else {
+          all[i].style.backgroundColor = "#a5e0e4";
+        }
+      }
+      console.log(all.length);
+    });
+    // document.getElementById("content").style.backgroundColor = "blue";
+    // document.getElementById("mw-head-base").style.backgroundColor = "yellow";
   }
