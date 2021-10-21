8590904b0dcf80bb15626c28f5caea72bd886e82
Thaddeus Biernacki <69869699+Thaddeus-Biernacki@users.noreply.github.com>
2021-10-21 09:26:33 +0200

    Update background.js

--- chrrome extension/background.js
+++ chrrome extension/background.js
@@ -1,21 +1,13 @@
-
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (!message.run) return;
 
     const url = message.url;
     console.log('Scraping url: ' + url);
-    const apiCall = 'http://localhost:8080/api/scrape?url=' + url;
-    console.log('API call made: ' + apiCall);
-    fetch(apiCall).then( function(res) {
       //wait for response
-      if (res.status !== 200) {
-        console.log({name:'Error', num_par: '0'});
-        return;
-      }
       res.json().then(function(data) {
         console.log(data);
         //data is the list of jsons
-        const newCall = 'http://localhost:8080/api/getPredictions';
+        const newCall = 'http://localhost:8080/api/getPredictions?url=' + url;
         console.log("get results");
         fetch(newCall).then(function(response) {
           if (response.status !== 200) {
@@ -28,6 +20,5 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
           });
         });
       });
-    });
     return true;
 });
