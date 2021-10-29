8f5d1a05e4ac0d694c5182d7c95c72b87672934c
Hannah Rosenstrauch <21970610@sun.ac.za>
2021-10-29 12:42:47 +0200

    extension updated

--- chrrome extension/popup.js
+++ chrrome extension/popup.js
@@ -2,6 +2,7 @@
 let colorDivs = document.getElementById("colorDivs");
 
 colorDivs.addEventListener("click", async () => {
+  Hide();
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({
@@ -21,10 +22,24 @@ colorDivs.addEventListener("click", async () => {
         for (var k = 0, maximum = elements.length; k < maximum; k++) {
           console.log(response[i]);
           elements[k].style.backgroundColor = "blue";
+          document.getElementById("list").innerHTML += 
+            "<li>" + response[i] + "</li>";
         }
       }
       console.log(response.length);
     });
+
     // document.getElementById("content").style.backgroundColor = "blue";
     // document.getElementById("mw-head-base").style.backgroundColor = "yellow";
+  }
+
+  function Hide() {
+    document.getElementById("list").innerHTML += 
+        "<li>Adele</li>";
+    document.getElementById("list").innerHTML += 
+        "<li>bdele</li>";
+    document.getElementById("list").innerHTML += 
+        "<li>Ab</li>";
+    document.getElementById("paragraph").style.display = "none";
+    document.getElementById("colorDivs").style.display = "none";
   }
